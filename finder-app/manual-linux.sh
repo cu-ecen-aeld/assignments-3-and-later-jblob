33f48ce211a5debd2180cd6ea76a81b535cd04d3
jblob <juergen.blob@cariad.technology>
2026-04-10 07:38:53 +0200

    assignment3part2

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 # Script outline to install and build kernel.
-# Author: Siddhant Jajoo.
 
 set -e
 set -u
 
 OUTDIR=/tmp/aeld
-KERNEL_REPO=git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git
+KERNEL_REPO=https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git
 KERNEL_VERSION=v5.15.163
 BUSYBOX_VERSION=1_33_1
 FINDER_APP_DIR=$(realpath $(dirname $0))
 ARCH=arm64
 CROSS_COMPILE=aarch64-none-linux-gnu-
+STARTDIR=`pwd`
 
 if [ $# -lt 1 ]
 then
@@ -32,6 +32,7 @@ cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/linux-stable" ]; then
     #Clone only if the repository does not exist.
 	echo "CLONING GIT LINUX STABLE VERSION ${KERNEL_VERSION} IN ${OUTDIR}"
+	echo "git clone ${KERNEL_REPO} --depth 1 --single-branch --branch ${KERNEL_VERSION}"
 	git clone ${KERNEL_REPO} --depth 1 --single-branch --branch ${KERNEL_VERSION}
 fi
 if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
@@ -39,47 +40,100 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     echo "Checking out version ${KERNEL_VERSION}"
     git checkout ${KERNEL_VERSION}
 
-    # TODO: Add your kernel build steps here
+    # kernel build steps
+    #export CROSS_COMPILE=aarch64-none-linux-gnu-
+    #export ARCH=arm64
+
+    # 1. Clean up everything (Only needed for a totally fresh start)
+    # make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper
+
+    # 2. Generate the ARM64 configuration
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
+    
+    # 3. Build the Kernel, Modules, and Device Trees
+    make -j4 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all
+    #make -j4 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} Image modules dtbs
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} modules # Build any kernel modules
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} dtbs # Build the devicetree
 fi
 
+# copy the generated image to outdir
 echo "Adding the Image in outdir"
+cp ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ${OUTDIR}
+
 
 echo "Creating the staging directory for the root filesystem"
 cd "$OUTDIR"
 if [ -d "${OUTDIR}/rootfs" ]
 then
-	echo "Deleting rootfs directory at ${OUTDIR}/rootfs and starting over"
-    sudo rm  -rf ${OUTDIR}/rootfs
+    echo "Deleting rootfs directory at ${OUTDIR}/rootfs and starting over"
+    sudo rm -rf ${OUTDIR}/rootfs
 fi
 
 # TODO: Create necessary base directories
+cd "$OUTDIR"
+mkdir -p rootfs
+cd rootfs
+mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
+mkdir -p usr/bin usr/lib usr/sbin
+mkdir -p var/log
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
 then
-git clone git://busybox.net/busybox.git
+    git clone https://git.busybox.net/busybox
     cd busybox
     git checkout ${BUSYBOX_VERSION}
-    # TODO:  Configure busybox
+    # Configure busybox
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
+    # sed -i 's/# CONFIG_STATIC is not set/CONFIG_STATIC=y/' .config
 else
     cd busybox
 fi
 
-# TODO: Make and install busybox
+# Make and install busybox
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} 
+make CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
+
+# Add library dependencies to rootfs
+cd "${OUTDIR}/rootfs"
 
 echo "Library dependencies"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
-# TODO: Add library dependencies to rootfs
+SYSROOT=$(${CROSS_COMPILE}gcc -print-sysroot)
+LOADER=$(${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter" | awk -F': ' '{print $2}' | tr -d ']')
+#echo "LOADER=${LOADER}"
+cp -L $SYSROOT/$LOADER $OUTDIR/rootfs/$LOADER
 
-# TODO: Make device nodes
+LIBS=$(${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library" | awk -F'[][]' '{print $2}')
+#echo "LIBS=${LIBS}"
+for lib in $LIBS; do
+    find $SYSROOT -name "$lib" -exec cp -L {} $OUTDIR/rootfs/lib64/ \;
+done
 
-# TODO: Clean and build the writer utility
+# Make device nodes
+sudo mknod -m 666 dev/null c 1 3
+sudo mknod -m 600 dev/console c 5 1
 
-# TODO: Copy the finder related scripts and executables to the /home directory
+# Clean and build the writer utility
+cd $STARTDIR
+make clean
+make CROSS_COMPILE=${CROSS_COMPILE}
+
+# Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
+cp writer ${OUTDIR}/rootfs/home
+cp finder.sh ${OUTDIR}/rootfs/home
+cp finder-test.sh ${OUTDIR}/rootfs/home
+cp -r ../conf ${OUTDIR}/rootfs/home
+sed -i 's|\.\./conf/|conf/|' ${OUTDIR}/rootfs/home/finder-test.sh
+cp autorun-qemu.sh ${OUTDIR}/rootfs/home
 
-# TODO: Chown the root directory
+# Chown the root directory
+cd "${OUTDIR}/rootfs"
+sudo chown -R root:root *
 
-# TODO: Create initramfs.cpio.gz
+# Create initramfs.cpio.gz
+find . | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
+gzip -f ${OUTDIR}/initramfs.cpio
+
