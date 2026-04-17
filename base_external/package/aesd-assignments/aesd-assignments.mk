eaad8d47c68945bea9542e81e9e42426a4487f84
jblob <juergen.blob@cariad.technology>
2026-04-17 11:05:05 +0200

    fixes

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -18,6 +18,8 @@ AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 define AESD_ASSIGNMENTS_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server all
+	# removes hidden Windows line endings
+	sed -i 's/\r$$//' $(@D)/server/aesdsocket-start-stop
 endef
 
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
