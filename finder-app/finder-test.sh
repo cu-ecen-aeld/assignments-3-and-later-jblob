404d3261c49643da46308d6f2d411ff56cd8c230
jblob <juergen.blob@cariad.technology>
2026-04-23 21:20:21 +0200

    Final Assignment 3 fix: confirmed local success after env cleanup

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -8,8 +8,7 @@ set -u
 NUMFILES=10
 WRITESTR=AELD_IS_FUN
 WRITEDIR=/tmp/aeld-data
-CONFDIR=/etc/finder-app/conf
-username=$(cat ${CONFDIR}/username.txt)
+username=$(cat conf/username.txt)
 
 if [ $# -lt 3 ]
 then
@@ -33,7 +32,7 @@ echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}"
 rm -rf "${WRITEDIR}"
 
 # create $WRITEDIR if not assignment1
-assignment=`cat ${CONFDIR}/assignment.txt`
+assignment=`cat ../conf/assignment.txt`
 
 if [ $assignment != 'assignment1' ]
 then
@@ -55,10 +54,10 @@ fi
 
 for i in $( seq 1 $NUMFILES)
 do
-	writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
+	./writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
 
-OUTPUTSTRING=$(finder.sh "$WRITEDIR" "$WRITESTR" > /tmp/assignment4-result.txt)
+OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
 
 # remove temporary directories
 rm -rf /tmp/aeld-data
