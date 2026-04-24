93815ef639c1ef9223d0493434debd77eb40e040
jblob <juergen.blob@cariad.technology>
2026-04-24 14:30:13 +0200

    Update commit hash

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,8 +6,8 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-###AESD_ASSIGNMENTS_VERSION = 761af1cd3e17f19db7e73b38f2bb069b5ad9124e
-AESD_ASSIGNMENTS_VERSION = main
+AESD_ASSIGNMENTS_VERSION = 1d6033d5d5c665f518f3a33f919dc8fcb8e7f422
+###AESD_ASSIGNMENTS_VERSION = main
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
