058851a5724b6768bb48e83f4df0fc8565fc80c9
jblob <juergen.blob@cariad.technology>
2026-05-12 13:52:34 +0200

    fixed qemu start script

--- runqemu.sh
+++ runqemu.sh
@@ -2,5 +2,5 @@
 
 # Start Yocto QEMU correctly
 source poky/oe-init-build-env
-runqemu qemuarm64 nographic slirp hostfwd=tcp::10022-:22
+runqemu nographic slirp hostfwd=tcp::10022-:22
 #runqemu qemux86-64 nographic slirp
